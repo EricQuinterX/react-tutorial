8cec9f561b300908a95269863256b57d51f0cf78
EricQuinterX <eric.brian.quinteros@gmail.com>
2018-12-24 00:14:01 -0300

    Evento onClick

--- react-guide/src/App.js
+++ react-guide/src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Eric", age: 26 },
+      { name: "Kevin", age: 25 },
+      { name: "Melissa", age: 23 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('Was clicked');
+  }
+
   render() {
+    const persons = this.state.persons;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={persons[0].name} age={persons[0].age} />
+        <Person name={persons[1].name} age={persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={persons[2].name} age={persons[2].age} />
       </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
 }
